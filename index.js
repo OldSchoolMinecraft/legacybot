ae121538906aa76021d198d6fc3f43a3eac84c9f
moderatorman <12193288+moderatorman@users.noreply.github.com>
2021-11-26 11:12:47 -0800

    Update config

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const { token, sql_host, sql_user, sql_pass } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
+const mysql = require("mysql");
 
 client.once('ready', () =>
 {
@@ -17,9 +18,12 @@ client.on('messageCreate', message =>
         {
             message.reply("Invalid arguments. Usage: `!link USERNAME`").catch(console.error);
             return;
-        }
+        } else
+            console.log(`${message.author.tag} issued command: ${message.content}`);
     }
 });
+
+function
 
 function lwstrcmp(var1, var2)
 {
